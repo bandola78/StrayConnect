e7898c06abf13693c9b69be067fee49b456358dc
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-11-09 20:31:22 +0800

    Update StrayAnimal.java

---
+++ src/models/StrayAnimal.java
@@ -0,0 +1,74 @@
+package models;
+
+import models.AnimalAttributes.Gender;
+import models.AnimalAttributes.Species;
+import models.AnimalAttributes.Size;
+
+import java.time.LocalDate;
+
+public class StrayAnimal extends Animal {
+    private String locationFound;
+    private LocalDate dateRescued;
+    private String rescuerName;
+    private String rescuerContact;
+
+    public StrayAnimal(String name, Gender gender, Species species, Enum<?> color, int approxAge,
+                       Size size, boolean isVaccinated, String healthStatus, String locationFound,
+                       LocalDate dateRescued, String rescuerName, String rescuerContact) 
+                       {
+        super(name, gender, species, color, approxAge, size, isVaccinated, healthStatus);
+        this.locationFound = locationFound;
+        this.dateRescued = dateRescued;
+        this.rescuerName = rescuerName;
+        this.rescuerContact = rescuerContact;
+    }
+
+    public String getLocationFound() { 
+        return locationFound; 
+        }
+
+    public void setLocationFound(String locationFound) { 
+        this.locationFound = locationFound; 
+        }
+
+    public LocalDate getDateRescued() { 
+        return dateRescued; 
+        }
+
+    public void setDateRescued(LocalDate dateRescued) { 
+        this.dateRescued = dateRescued; 
+        }
+
+    public String getRescuerName() { 
+        return rescuerName; 
+        }
+
+    public void setRescuerName(String rescuerName) { 
+        this.rescuerName = rescuerName; 
+        }
+
+    public String getRescuerContact() { 
+        return rescuerContact; 
+        }
+
+    public void setRescuerContact(String rescuerContact) { 
+        this.rescuerContact = rescuerContact; 
+        }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Stray Animal - Name: " + getName());
+        System.out.println("Gender: " + getGender());
+        System.out.println("Species: " + getSpecies())
+        System.out.println("Color: " + getColor())
+        System.out.println("Age (Approximate): " + getApproxAge());
+        System.out.println("Size:" + getSize("S" : "M" : "L") )
+        System.out.println("Vaccinated: " + (isVaccinated() ? "Yes" : "No"));
+        System.out.println("Health Status: " + getHealthStatus());
+
+        System.out.println("Location Found: " + getlocationFound("Landmark/Barangay/City"))
+        System.out.println("Date Rescued: " + getdateRescued("MM/DD/YY"));
+        System.out.println("Rescuer: " + getrescuerName())
+        System.out.println("Contact: " + getrescuerContact());
+    }
+}
