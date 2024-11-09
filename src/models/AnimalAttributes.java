bb997187729964f2583c6c5b20c4c74f5860ef85
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-11-09 20:29:49 +0800

    Update AnimalAttributes.java

---
+++ src/models/AnimalAttributes.java
@@ -0,0 +1,58 @@
+package models;
+
+public class AnimalAttributes {
+
+    public enum Species {
+        CAT,
+        DOG
+    }
+
+    public enum Gender {
+        MALE,
+        FEMALE,
+        M,
+        F
+    }
+
+    public enum DogColor {
+        BLACK,
+        BROWN,
+        WHITE,
+        GOLDEN,
+        GRAY,
+        MIXED
+    }
+
+    public enum CatColor {
+        BLACK,
+        WHITE,
+        GRAY,
+        ORANGE,
+        TABBY,
+        CALICO,
+        TORTOISESHELL,
+        MIXED
+    }
+
+    public enum GeneralColor {   
+        BLACK,
+        WHITE,
+        BROWN,
+        GRAY,
+        ORANGE,
+        GOLDEN,
+        TABBY,
+        CALICO,
+        TORTOISESHELL,
+        MIXED
+    }
+
+    public enum Size {
+        SMALL,
+        MEDIUM,
+        LARGE,
+        S,
+        M,
+        L
+    }
+}
