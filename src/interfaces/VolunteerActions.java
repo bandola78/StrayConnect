7b128e4ce025399152bd1e56ac2d25b9490f776f
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-12-13 11:09:05 +0800

    Update VolunteerActions.java

--- src/interfaces/VolunteerActions.java
+++ src/interfaces/VolunteerActions.java
@@ -1,1 +1,18 @@
+package interfaces;
 
+import models.StrayAnimal;
+
+public interface VolunteerActions {
+
+    /**
+     * Registers the volunteer's rescue activity.
+     * 
+     * @param animal The stray animal being rescued.
+     */
+    void registerRescue(StrayAnimal animal);
+
+    /**
+     * Displays the volunteer's activities and contributions.
+     */
+    void displayVolunteerContributions();
+}
