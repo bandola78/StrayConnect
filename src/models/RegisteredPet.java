a66b6cb381b1ef5504ba45f7ab979478f7327ea6
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-12-13 10:48:20 +0800

    Update RegisteredPet.java

--- src/models/RegisteredPet.java
+++ src/models/RegisteredPet.java
@@ -26,12 +26,12 @@ public class RegisteredPet extends Animal {
     public void displayInfo() {
         System.out.println("Registered Pet - Name: " + getName());
         System.out.println("Gender: " + getGender());
-        System.out.println("Species: " + getSpecies())
-        System.out.println("Color: " + getColor())
+        System.out.println("Species: " + getSpecies());
+        System.out.println("Color: " + getColor());
         System.out.println("Age (Approximate): " + getApproxAge());
-        System.out.println("Size: " + getSize("S" : "M" : "L"))
+        System.out.println("Size: " + getSize());
         System.out.println("Vaccinated: " + (isVaccinated() ? "Yes" : "No"));
         System.out.println("Health Status: " + getHealthStatus());
-        System.out.println("Owner: " + getOwnerName())
+        System.out.println("Owner: " + getOwnerName());
     }
 }
