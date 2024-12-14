1b9c08cf32a788aa77b6d73fab79249afd7870b9
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-12-14 12:25:27 +0800

    Update RegisteredPet.java

--- src/models/RegisteredPet.java
+++ src/models/RegisteredPet.java
@@ -4,23 +4,43 @@ import models.AnimalAttributes.Gender;
 import models.AnimalAttributes.Species;
 import models.AnimalAttributes.Size;
 
-
 public class RegisteredPet extends Animal {
     private String ownerName;
+    private String contactNumber;
+    private String address;
 
     public RegisteredPet(String name, Gender gender, Species species, Enum<?> color, int approxAge,
-                         Size size, boolean isVaccinated, String healthStatus, String ownerName) {
+                         Size size, boolean isVaccinated, String healthStatus, String ownerName,
+                         String contactNumber, String address) {
         super(name, gender, species, color, approxAge, size, isVaccinated, healthStatus);
+        this.ownerName = ownerName;
+        this.contactNumber = contactNumber;
+        this.address = address;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
     }
 
-    public String getOwnerName() { 
-        return ownerName; 
-        }
+    public String getContactNumber() {
+        return contactNumber;
+    }
 
-    public void setOwnerName(String ownerName) { 
-        this.ownerName = ownerName; 
-        }
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     @Override
     public void displayInfo() {
@@ -33,5 +53,7 @@ public class RegisteredPet extends Animal {
         System.out.println("Vaccinated: " + (isVaccinated() ? "Yes" : "No"));
         System.out.println("Health Status: " + getHealthStatus());
         System.out.println("Owner: " + getOwnerName());
+        System.out.println("Contact Number: " + getContactNumber());
+        System.out.println("Address: " + getAddress());
     }
 }
