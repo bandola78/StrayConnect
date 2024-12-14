c369686492362820b4e24b7752321e90adcf5f75
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-12-14 12:29:49 +0800

    Update InputHelper.java

--- src/utils/InputHelper.java
+++ src/utils/InputHelper.java
@@ -50,4 +50,24 @@ public class InputHelper {
             }
         }
     }
-}
+
+    public static void pause(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            System.out.println("An error occurred during the pause.");
+        }
+    }
+    public static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            System.out.println("\n\n\n\n\n\n\n\n\n\n"); 
+        }
+    }
+}    
